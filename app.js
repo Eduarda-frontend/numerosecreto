ad4781b78f1123ccb8c5848b952ab08cf2152fd3
Maria Eduarda <134226412+Eduarda-frontend@users.noreply.github.com>
2023-11-15 18:35:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-const numeroSecreto = parseInt (Math.random()  * 1000) +1;
-console.log(numeroSecreto);
+const numeroSecreto = parseInt(Math.random() * 1000) + 1;
 let nome = prompt('Qual o seu nome?');
 let tentativas = 1;
 let tentativasRestantes = 10;
 alert(`Fique calmo, estamos gerando um novo número para você!`)
 
 
-for (let contador = 0; tentativas < 10; tentativas++) {
+for (tentativas; tentativas <= 10; tentativas++) {
 
-    let palpite = nome + prompt(`${nome}, adivinhe um número de 1 a 1000:`);
+    let palpite = prompt(`${nome}, adivinhe um número de 1 a 1000:`);
 
-    if (palpite == numeroSecreto) {
+    if (palpite <= numeroSecreto) {
+
+        alert(`${nome}, o número secreto é maior do que ${palpite}!`);
+        tentativasRestantes-- 
+
+    } else if (palpite >= numeroSecreto) {
+
+        alert(`${nome}, o número secreto é menor do que ${palpite}!`);
+        tentativasRestantes-- 
+
+    } else if (palpite == numeroSecreto) {
+
         alert(`Parabéns você acertou! O número secreto é ${numeroSecreto}`);
         break;
-    } else if (palpite >= numeroSecreto) {
-        alert(`${nome}, o número secreto é menor do que ${palpite}!`);
-    } else if (palpite <= numeroSecreto) {
-        alert(`${nome}, o número secreto é maior do que ${palpite}!`);
     }
 
-    tentativasRestantes--
-
-    const palavraTentativas = tentativas > 1?"tentativas":"tentativa";
-    alert(`Você já teve ${tentativas} ${palavraTentativas}, restam apenas ${tentativasRestantes}.` )
+    const palavraTentativas = tentativas > 1 ? "tentativas" : "tentativa";
+    alert(`Você já teve ${tentativas} ${palavraTentativas}, restam apenas ${tentativasRestantes}.`);
 
 }
 
